ec51cfa35abd6c3f1bb361175721851aedbb8fb4
张显磊 <1079105171@qq.com>
2020-01-02 18:34:04 +0800

    搭建好框架

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import List from './page/ToDoList.jsx';
+
+import { Button, Drawer } from 'antd';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'antd/dist/antd.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Header extends React.Component {
+    render() {
+        return <>
+            <div style={{ width: '100%', height: '60px', backgroundColor: '#333' }}>
+                <img src='./image/jieni.jpg' style={{ width: '45px', height: '45px', marginTop: '7.5px', marginLeft: '30px' }} alt="icon"></img>
+                <span style={{ lineHight: '60px', fontSize: '40px', color: '#fff', verticalAlign: 'top', marginLeft: '40%' }}>ToDoList</span>
+                <Button type="primary" onClick={() => this.props.addEvent()} style={{height:'40px',position:'fixed',top:'10px',right:'30px'}}>添加事项</Button>
+            </div>
+        </>
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+    state = {
+        visiable:false
+    }
+
+    addEvent(){
+        this.setState({
+            visiable:true
+        })
+    }
+
+    closeDrawer(){
+        this.setState({
+            visiable:false 
+        })
+    }
+
+    render() {
+        return <>
+            <Header addEvent={this.addEvent.bind(this)}></Header>
+
+            <List></List>
+
+            <Drawer title="添加事项" placement="bottom" closable={false} onClose={this.closeDrawer.bind(this)} visible={this.state.visiable}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Drawer>
+        </>
+    }
+}
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ src/page/ToDoList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class List extends React.Component{
+
+    render(){
+        return <>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </>
+    }
+}
+export default List;
